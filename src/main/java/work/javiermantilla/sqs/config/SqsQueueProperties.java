7f3033628b0b2b189e2ce294f33ba414d33e3cc9
Javier Orlando Mantilla Portilla <jomantil@bancolombia.com.co>
2025-07-29 16:49:25 -0500

    Se agrega anotacion diciente

--- src/main/java/work/javiermantilla/sqs/config/SqsQueueProperties.java
+++ src/main/java/work/javiermantilla/sqs/config/SqsQueueProperties.java
@@ -3,13 +3,13 @@ package work.javiermantilla.sqs.config;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @NoArgsConstructor
 @Data
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "aws.sqs.queue")
 public class SqsQueueProperties {
     private String name;
